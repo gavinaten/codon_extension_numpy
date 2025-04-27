2c894221ca75d49e3e38518b2d08d7d68e22c81f
Gavin Aten <gavin.aten@upstart.com>
2025-04-27 15:01:24 -0400

    numpy array return type issue

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-# def example_method_np(array1: np.ndarray) -> float:
-#     return sum(array1)
+def example_method_ndarray() -> np.ndarray:
+    return np.array([1.0, 2.0], dtype=np.float64)
 
-def example_method_list(array1: list[float]) -> float:
-    return sum(array1)
+# def example_method_list() -> list[np.float64]:
+#     return [1.0, 2.0]
